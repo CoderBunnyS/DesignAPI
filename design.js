19c79bd3ffe4ef4b5a42fceadac5e1ff0ea82178
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2020-10-11 10:53:25 -0700

    final commit - I ACTUALLY FINISHED A PROJECT

--- design.js
+++ design.js
@@ -1,68 +1,31 @@
-const prints = "https://api.harvardartmuseums.org/object?page=1571&apikey=69722951-0855-4881-85a9-5f645f0a3b6f&size=1&hasimage=1&imagepermissionlevel=0&classification=Paintings"
-const sculpture = "https://api.harvardartmuseums.org/object?page=1571&apikey=69722951-0855-4881-85a9-5f645f0a3b6f&size=1&hasimage=1&imagepermissionlevel=0&classification=Sculpture"
-const jewelry = "https://api.harvardartmuseums.org/object?page=1571&apikey=69722951-0855-4881-85a9-5f645f0a3b6f&size=1&hasimage=1&imagepermissionlevel=0&classification=Photographs"
+const prints = "https://api.harvardartmuseums.org/object?apikey=69722951-0855-4881-85a9-5f645f0a3b6f&hasimage=1"
 
+fetch(prints)
+  .then(res => res.json())
+  .then(res => console.log("success!", res))
+  .catch(err => console.log("something went wrong...", err));
 
-// fetch(sculpture) 
-//     .then(res => res.json())
-//     .then(response=> {
-//         console.log(response.records[0].url)
-//     })
 
     fetch(prints)
     .then(response => response.json())
     .then(response => {
  //       console.log(response)
         resPrints = response
-        let randomImage = document.querySelector("#randomImage")
+        let randomPrint = document.querySelector("#randomPrint")
         for(i=0; i<resPrints.records.length; i++){
-//            console.log(resPrints.records[i].primaryimageurl)
+            console.log(resPrints)
             let printsElement = document.createElement("img")
-            printsElement.setAttribute("src", resPrints.records[i].primaryimageurl)
-            printsElement.setAttribute("width", 250)
-            printsElement.setAttribute("height", 200)
-            randomImage.appendChild(printsElement)
-        }
-    })
-
-
-
-
-    fetch(sculpture)
-    .then(response => response.json())
-    .then(response => {
- //       console.log(response)
-        sculptures = response
-        let randomSculpture = document.querySelector("#randomSculpture")
-        for(i=0; i<sculptures.records.length; i++){
-//            console.log(sculptures.records[0].url)
-            let sculptureElement = document.createElement("img")
-            sculptureElement.setAttribute("src", sculptures.records[i].primaryimageurl)
-            sculptureElement.setAttribute("width", 350)
-            sculptureElement.setAttribute("height", 400)
-            randomSculpture.appendChild(sculptureElement)
-        }
-    })
-
-
-
-    fetch(jewelry)
-    .then(response => response.json())
-    .then(response => {
-        jewels = response
-        let randomJewelry = document.querySelector("#randomJewelry")
-        for(i=0; i<jewels.records.length; i++){
-//            console.log(jewels.records[i].baseimageurl)
-            let jewelElement = document.createElement("img")
-            jewelElement.setAttribute("src", jewels.records[i].primaryimageurl)
-            jewelElement.setAttribute("width", 250)
-            jewelElement.setAttribute("height", 200)
-            randomJewelry.appendChild(jewelElement)
+            if (resPrints.records[i].images.length>0){
+                printsElement.setAttribute("src", resPrints.records[i].images[0].baseimageurl)
+                printsElement.setAttribute("width", 250)
+                printsElement.setAttribute("height", 200)
+                randomPrint.appendChild(printsElement)
+            }
+ 
         }
     })
 
 var accordion = document.querySelectorAll(".accordion");
-//console.log(accordion)
 var i
 
 for (i=0; i< accordion.length; i++) {
@@ -72,9 +35,7 @@ for (i=0; i< accordion.length; i++) {
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
+            panel.style.maxHeight = panel.scrollHeight + 500 + "px";
         }
     })
 }
-//let personImage = document.querySelector(".personImage")
-
